ec6fd10c6b5d1380241137602d6dcf08a92d6cff
Priyanshu Kumar Singh <pks9918705@gmail.com>
2024-07-16 14:10:24 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom"
+import Wifi from "./wifi"
+import DistanceControlScreen from "./DistanceControlScreen"
+import DistanceLock from "./DistanceLock"
+ 
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={ <Wifi/> } />
+        <Route path="/control" element={ <DistanceControlScreen/> } />
+        <Route path="/lock" element={<DistanceLock/>}/>
+         
+      </Routes>
+    </div>
+  )
+}
+
+export default App
